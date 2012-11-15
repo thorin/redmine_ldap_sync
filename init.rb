cff9ff417c8bae3df26bd1a7a9d676e51a31b06d
thorin <thorin.iv@gmail.com>
2012-11-15 05:44:44 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_ldap_sync do
   author_url 'mailto:Ricardo Santos <ricardo.santos@vilt-group.com>?subject=redmine_ldap_sync'
   description 'Syncs users and groups with ldap'
   url 'https://github.com/thorin/redmine_ldap_sync'
-  version '1.2.0'
+  version '1.3.0'
   requires_redmine :version_or_higher => '2.0.0'
 
   
