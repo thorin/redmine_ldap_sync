77fd50856cc556acf4f06935748a2bbb1a10ce5e
Thorin <thorin.iv@gmail.com>
2013-02-28 19:26:13 +0100

    Groups get created on dryrun (#64)

--- lib/redmine_ldap_sync/redmine_ext/user_dry_run.rb
+++ lib/redmine_ldap_sync/redmine_ext/user_dry_run.rb
@@ -2,26 +2,40 @@ module RedmineLdapSync
   module RedmineExt
     module UserDryRun
 
-      def self.included(base)
-
-        base.class_eval do
-          def lock!; end
-
-          def activate!; end
+      module ClassMethods
+        def create(attributes)
+          user = User.new(attributes)
+          yield user if block_given?
+          user
         end
+      end
 
-        base.instance_eval do
+      module InstanceMethods
+        def lock!();end
+
+        def activate!(); end
+
+        def update_attributes(attrs = {}); end
+
+        def save(*args); end
+      end
+
+      def self.included(receiver)
+        receiver.extend(ClassMethods)
+        receiver.send(:include, InstanceMethods)
+
+        receiver.instance_eval do
           has_and_belongs_to_many :groups do
             def <<(groups)
-              puts "   !! Adding groups '#{groups.map(&:lastname).join("', '")}'" unless groups.empty?
+              puts "   !! Added to groups '#{groups.map(&:lastname).join("', '")}'" unless groups.empty?
+            end
+
+            def delete(*groups)
+              puts "   !! Removed from groups '#{groups.map(&:lastname).join("', '")}'" unless groups.empty?
             end
           end
 
-          def create(attributes)
-            user = User.new(attributes)
-            yield user if block_given?
-            user
-          end
+          remove_method :lock!, :activate!
         end
 
       end
