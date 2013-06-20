aec05a5f43f2bb0260cefb8efc08058217f7d30f
Thorin <thorin.iv@gmail.com>
2013-06-20 17:52:54 +0100

    version 2.0.1

--- lib/ldap_sync/version.rb
+++ lib/ldap_sync/version.rb
@@ -1,3 +1,20 @@
+# encoding: utf-8
+# Copyright (C) 2011-2013  The Redmine LDAP Sync Authors
+#
+# This file is part of Redmine LDAP Sync.
+#
+# Redmine LDAP Sync is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Redmine LDAP Sync is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Redmine LDAP Sync.  If not, see <http://www.gnu.org/licenses/>.
 module LdapSync
   module VERSION #:nodoc:
     MAJOR = 2
@@ -8,7 +25,7 @@ module LdapSync
     # * official release: nil
     # * stable branch:    stable
     # * trunk:            devel
-    BRANCH = 'devel'
+    BRANCH = nil
 
     # Retrieves the revision from the working copy
     def self.revision
