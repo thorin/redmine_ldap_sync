c6beb42cd243b8cc771ba1f6dea09296ec891dd5
thorin <thorin.iv@gmail.com>
2013-11-18 17:57:35 +0000

    Update ldap_settings_controller.rb

--- app/controllers/ldap_settings_controller.rb
+++ app/controllers/ldap_settings_controller.rb
@@ -74,13 +74,12 @@ class LdapSettingsController < ApplicationController
   # GET /ldap_settings/1/test
   def test
     if @ldap_setting.valid?
-      @test = LdapTest.new(@ldap_setting)
-
       users   = params[:ldap_test][:test_users] || ''
       groups  = params[:ldap_test][:test_groups] || ''
       users   = users.split(',').map(&:strip).reject(&:blank?)
       groups  = groups.split(',').map(&:strip).reject(&:blank?)
 
+      @test = LdapTest.new(@ldap_setting)
       @test.run_with_users_and_groups(users, groups)
     else
       render 'test_error'
